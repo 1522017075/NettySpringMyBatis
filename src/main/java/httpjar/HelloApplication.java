3fb1d7b92b5f02a27873a796cc71ee51885adb9f
Aomrsou <1522017075@qq.com>
2021-08-20 15:49:36 +0800

    Update HelloApplication.java

--- src/main/java/httpjar/HelloApplication.java
+++ src/main/java/httpjar/HelloApplication.java
@@ -2,15 +2,11 @@ package httpjar;
 
 import com.blade.Blade;
 
-import httpjar.timetask.MyTimeTask;
-
 /*
  * 项目打jar包只能在项目根目录mvn clean install进行打包，因为spring-context与spring-aop包命名空间冲突，已在pom配置plugin打包,合并命名空间内文件解决之
  */
 public class HelloApplication {
 	public static void main(String[] args) {
-		// 启动定时任务
-		MyTimeTask.cron();
 	    Blade.of().get("/", ctx -> ctx.text("Hello Blade")).start();
 	}
 }
